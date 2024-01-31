b678af80460c8c08400131c47b3eaac871ad6bdb
swaraj Saxena <swarishraj00@gmail.com>
2024-02-01 01:29:44 +0530

    change in app

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const helmet = require('helmet');
-const { ErrorResponseObject } = require('./common/http');
-const routes = require('./routes');
+const express = require('express')
+const helmet = require('helmet')
+const { ErrorResponseObject } = require('./common/http')
+const routes = require('./routes')
 
-const app = express();
+const app = express()
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(helmet());
-app.use('/', routes);
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+app.use(helmet())
+app.use('/', routes)
 
 // default catch all handler
-app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
+app.all('*', (req, res) => res.status(404).json({ error: 'route not defined' }))
 
-module.exports = app;
+module.exports = app
